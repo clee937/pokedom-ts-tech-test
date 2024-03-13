c68d36ef2720e018bcc9cf291e0ee2e82f9706bc
Cheryl Lee <clee0937@gmail.com>
2024-03-13 11:41:13 +0000

    added function to filter by name and types

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,61 @@
 import "./styles/style.scss";
 import { pokemonArray } from "./data/pokemon.ts";
-
-console.log(pokemonArray);
+import { Pokemon } from "./data/types.ts";
 
 const cardContainer = document.querySelector<HTMLElement>(".card-container");
+const filterInput = document.querySelector<HTMLInputElement>(".cards__filter");
 
-if (!cardContainer) {
+if (!cardContainer || !filterInput) {
   throw new Error("Something went wrong with the query selector");
 }
 
-pokemonArray.forEach((pokemon) => {
-  if (pokemon.types.length === 1) {
-    cardContainer.innerHTML += `<div class="card">
-                                  <img class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}">
-                                  <div class="card__content">
-                                    <h2 class="card-_heading">${pokemon.name}</h2>
-                                    <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} type of pokemon.</p>
-                                  </div>
-                                  </div>`;
-  } else {
-    cardContainer.innerHTML += `<div class="card">
-                                  <img class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}">
-                                  <div class="card__content">
-                                    <h2 class="card-_heading">${pokemon.name}</h2>
-                                    <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} &  ${pokemon.types[1]} type of pokemon.</p>
-                                  </div>
-                                  </div>`;
-  }
-});
+let filteredPokemonCards = [...pokemonArray];
+
+const renderPokemonCards = () => {
+  cardContainer.innerHTML = "";
+  filteredPokemonCards.forEach((pokemon) => {
+    if (pokemon.types.length === 1) {
+      cardContainer.innerHTML += `<div class="card">
+                                    <img class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}">
+                                    <div class="card__content">
+                                      <h2 class="card-_heading">${pokemon.name}</h2>
+                                      <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} type of pokemon.</p>
+                                    </div>
+                                    </div>`;
+    } else {
+      cardContainer.innerHTML += `<div class="card">
+                                    <img class="card__image" src="${pokemon.sprite}" alt="${pokemon.name}">
+                                    <div class="card__content">
+                                      <h2 class="card-_heading">${pokemon.name}</h2>
+                                      <p class="card__text">${pokemon.name} (#${pokemon.id}) is a ${pokemon.types[0]} &  ${pokemon.types[1]} type of pokemon.</p>
+                                    </div>
+                                    </div>`;
+    }
+  });
+};
+
+const handleFilter = (event: Event) => {
+  const userInput = (
+    event.currentTarget as HTMLInputElement
+  ).value.toLowerCase();
+
+  filteredPokemonCards = pokemonArray.filter((pokemon: Pokemon) => {
+    if (pokemon.types[1]) {
+      return (
+        pokemon.name.toLowerCase().includes(userInput) ||
+        pokemon.types[0].toLowerCase().includes(userInput) ||
+        pokemon.types[1].toLowerCase().includes(userInput)
+      );
+    } else {
+      return (
+        pokemon.name.toLowerCase().includes(userInput) ||
+        pokemon.types[0].toLowerCase().includes(userInput)
+      );
+    }
+  });
+  renderPokemonCards();
+};
+
+renderPokemonCards();
+
+filterInput.addEventListener("input", handleFilter);
